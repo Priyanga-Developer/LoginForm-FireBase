ed999f4fd8a956f61e8a89cd74e565e5292114da
Priyanga <uthirapriyanga@gmail.com>
2023-11-14 21:21:59 +0530

    sign in  validation

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -5,18 +5,21 @@ import DataContext from './MyContext';
 
 
 const LoginPage = () => {
-  const {email,handleEmailChange,handlePwdChange,pwd,signIn,signWithGoogle}=useContext(DataContext);
+  const {email,handleEmailChange,handlePwdChange,pwd,signIn,signWithGoogle,error}=useContext(DataContext);
 
   return (
     <div className='LoginPage'>
 
             <form onClick={(e)=>e.preventDefault()} method='POST'>
+                    <span className='general'>{error.general}</span>
                     <div className="mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <span>{error.email}</span>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e)=>handleEmailChange(e)} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleInputPassword1"className="form-label">Password</label>
+                        <span>{error.password}</span>
                         <input type="password" className="form-control"  value={pwd}id="exampleInputPassword1" onChange={(e)=>handlePwdChange(e)}/>
                     </div>
                     <button type="submit" className="btn btn-primary" onClick={signIn} > Submit</button>
--- src/MyContext.js
+++ src/MyContext.js
@@ -12,6 +12,9 @@ export const MyContextProvider=({children})=>{
     
     const [email,setEmail]=useState("");
     const[pwd,setPwd]=useState("");
+    const[error,setError]=useState({});
+    // const[isCorrect,setIsCorrect]=useState(false);
+    const [isSubmit,setIsSubmit]=useState(false);
 
 
     
@@ -27,15 +30,42 @@ export const MyContextProvider=({children})=>{
     const navigate=useNavigate();
     const displayName=auth?.currentUser?.displayName;
     const signIn= async()=>{
-     
+  
+     setError(validate(email,pwd))
       try{
+        setIsSubmit(true);
         await signInWithEmailAndPassword(auth,email,pwd);
+       
         navigate("newpage");
+      
       }
       catch(err){
-        console.log(err.message);
+        if(isSubmit){
+          setError({general:"Email or password did not match"})
+          console.log(err);
+        }
+      
       }
     
+    }
+    const validate=()=>{
+      let err={}
+      const regex=/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+       if(!email && !pwd){
+          err.general="please fill all the fields";
+       }
+       else if(!pwd){
+         err.password="please fill password"
+       }
+       else if(!regex.test(email)){
+        err.email="please enter the valid email"
+       }
+       else if(pwd.length<6){
+        err.password="Password should atleast 6 characters"
+       }
+     
+      return err;
     }
     const signWithGoogle= async()=>{
       try{
@@ -59,11 +89,12 @@ export const MyContextProvider=({children})=>{
       catch(err){
         console.log(err)
       }
+  
     
     }
     
     return(
-    <MyContext.Provider value={{email,setEmail,pwd,setPwd,handleEmailChange,handlePwdChange,signWithGoogle,signIn,logOut,displayName}}>
+    <MyContext.Provider value={{email,setEmail,pwd,setPwd,handleEmailChange,handlePwdChange,signWithGoogle,signIn,logOut,displayName,error}}>
         {children}
     </MyContext.Provider>)
 }
